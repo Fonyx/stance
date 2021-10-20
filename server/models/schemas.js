af372cf5fae009d22b88e2e2ec441e5940998146
Nick <nick.alex.ritchie@gmail.com>
2021-10-20 22:53:33 +1100

    user seeding works

--- server/models/schemas.js
+++ server/models/schemas.js
@@ -230,7 +230,7 @@ const userSchema = new mongoose.Schema({
         unique: true,
         lowercase: true, 
         required: [true, "can't be blank"], 
-        match: [/^[a-zA-Z0-9]+$/, 'is invalid'], 
+        match: [/^[a-zA-Z0-9 -.]+$/, 'is invalid'], 
         index: true,
     },
     email: {
--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,8 +1,17 @@
+require('dotenv').config();
 const db = require('../config/connection');
 const { User } = require('../models');
 const userSeeds = require('./userSeeds.json');
+const Logger = require('../utils/logger');
 
 db.once('open', async () => {
+
+  if(process.env.MONGODB_URI){
+    Logger.info(`Seeding to mongo atlas fonxyOps STANCE instance`);
+  } else {
+    Logger.info('Seeding to backup mongo at local host')
+  }
+
   try {
     await User.deleteMany({});
 
