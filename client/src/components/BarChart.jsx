3a710f7a3162f15e6a37c66aee6380957c6bb205
Nick <nick.alex.ritchie@gmail.com>
2021-11-04 18:53:10 +1100

    basic circle demonstration of accounts

--- client/src/components/BarChart.jsx
+++ client/src/components/BarChart.jsx
@@ -6,51 +6,59 @@ import {select} from 'd3'
 
 export default function BarChart({accounts}) {
 
-    const [data, setData] = useState([ 2, 4, 2, 6, 8 ]);
+    // const [data, setData] = useState([ 2, 4, 2, 6, 8 ]);
     const svgDivContainer = useRef();
     const svgRef = useRef();
 
-    const updateData = () => {
-        console.log('Shuffling Data');
-        setData(data.map(value => value + Math.ceil(Math.random()*5)));
-    }
+    // const updateData = () => {
+    //     console.log('Shuffling Data');
+    //     setData(data.map(value => value + Math.ceil(Math.random()*5)));
+    // }
     
-    const popData = () => {
-        console.log('Pop Data');
-        setData(data.slice(1));
-    }
+    // const popData = () => {
+    //     console.log('Pop Data');
+    //     setData(data.slice(1));
+    // }
 
-    const pushData = () => {
-        console.log('Push Data');
-        let newData = Math.ceil(Math.random()*10);
-        setData([...data, newData]);
-    }
+    // const pushData = () => {
+    //     console.log('Push Data');
+    //     let newData = Math.ceil(Math.random()*10);
+    //     setData([...data, newData]);
+    // }
 
     const reDrawChart = () => {
         const svg = select(svgRef.current);
 
-        // svg.selectAll('circle').remove();
+        svg.selectAll('circle').remove();
 
         // add data to the svg using d3
         svg.selectAll('rect')
-            .data(data)
+            .data(accounts)
             .join(
                 // paint the circles on enter as orange
-                enter => enter.append('circle').attr('fill', 'orange'),
-                // paint the circles on enter as green
-                update => update.attr('fill', 'blue'),
+                enter => enter
+                    // .append('circle')
+                    // .attr('r', Math.random()*100)
+                    // .attr('cx', (d, i) => i*Math.random()*40)
+                    // .attr('cy', (d, i) => i*Math.random()*25)
+                    // .attr('fill', 'orange')
+                    .append('text')
+                    .attr('dx', () => 20)
+                    .attr('x', (d, i) => i*10)
+                    .attr('y', (d, i) => i*10)
+                    .text((d) => d.name),
+                // paint the circles on update as green
+                update => update.attr('fill', 'green'),
                 exit => exit.remove()
             )
-            .attr('r', value => value)
-            .attr('cx', (d) => d*Math.random()*40)
-            .attr('cy', (d) => d*Math.random()*25)
+            
     }
     
     useEffect(() => {
         reDrawChart();
-    },[data]);
+    },[]);
 
-    console.log('Data is: ', data)
+    console.log('Data is: ', accounts)
 
     return (
         <React.Fragment>
@@ -62,9 +70,9 @@ export default function BarChart({accounts}) {
                     ref={svgRef}
                 />
             </div>
-            <Button onClick={updateData}>Update Data</Button>
+            {/* <Button onClick={updateData}>Update Data</Button>
             <Button onClick={pushData}>Push Data</Button>
-            <Button onClick={popData}>Pop Data</Button>
+            <Button onClick={popData}>Pop Data</Button> */}
         </React.Fragment>
     );
 }
