c9bb5dbbf1df72a9865203829d679165d972ca16
Nick <nick.alex.ritchie@gmail.com>
2021-11-04 19:51:45 +1100

    example line chart

--- client/src/components/LineChart.jsx
+++ client/src/components/LineChart.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef} from 'react'
-import {select} from 'd3';
+import {select, csv, timeParse, scaleTime, extent, axisBottom, scaleLinear, axisLeft, max, line} from 'd3';
 
 export default function LineChart({transactions}) {
 
@@ -11,23 +11,55 @@ export default function LineChart({transactions}) {
 
         svg.selectAll('rect').remove();
 
-        // add data to the svg using d3
-        let blocks = svg.selectAll('rect')
-            .data(transactions)
-            .enter()
-            .append('g')
-            .attr('transform', (d) => "translate(10,80)")
+        // set the dimensions and margins of the graph
+        const margin = {top: 10, right: 30, bottom: 30, left: 60},
+            width = 460 - margin.left - margin.right,
+            height = 400 - margin.top - margin.bottom;
 
-        blocks.append('circle')
-            .attr('r', (d, i) => 10)
-            .attr('fill', 'orange')
-            .attr('cx', (d, i) => (i+1)*40)
-            .attr('cy', (d, i) => (i+1)*25)
-        
-        blocks.append('text')
-            .attr('dx', (d, i) => (i+1)*40)
-            .attr('dy', (d, i) => (i+1)*25)
-            .text((d) => d.description)
+        // append the svg object to the body of the page
+        svg.attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+            .attr("transform", `translate(${margin.left},${margin.top})`);
+
+        //Read the data
+        csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv",
+
+        // When reading the csv, I must format variables:
+        function(d){
+            return { date : timeParse("%Y-%m-%d")(d.date), value : d.value }
+        }).then(
+
+        // Now I can use this dataset:
+        function(data) {
+
+            // Add X axis --> it is a date format
+            const x = scaleTime()
+            .domain(extent(data, function(d) { return d.date; }))
+            .range([ 0, width ]);
+            svg.append("g")
+            .attr("transform", `translate(0, ${height})`)
+            .call(axisBottom(x));
+
+            // Add Y axis
+            const y = scaleLinear()
+            .domain([0, max(data, function(d) { return +d.value; })])
+            .range([ height, 0 ]);
+            svg.append("g")
+            .call(axisLeft(y));
+
+            // Add the line
+            svg.append("path")
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", "steelblue")
+            .attr("stroke-width", 1.5)
+            .attr("d", line()
+                .x(function(d) { return x(d.date) })
+                .y(function(d) { return y(d.value) })
+                )
+
+        })
 
     }
 
